ca4442ef79abf75ab24ab977f480a14b2dc6e328
tiagofinger <tiagocfinger@gmail.com>
2016-05-22 18:36:13 -0300

    Add
    add files of example

---
+++ example_get_data_nutrient.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+ * Example get data nutrient
+ */
+$url = 'http://'.$_SERVER['SERVER_NAME'].'/'.basename(__DIR__).'/web/nutrient/204,205';
+
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $url,
+    CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6 (.NET CLR 3.5.30729)'
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
+echo $resp;
---
+++ example_post_data.php
@@ -0,0 +1,42 @@
+<?php
+/* 
+ * Saving data example
+ */
+$url = 'http://'.$_SERVER['SERVER_NAME'].'/'.basename(__DIR__).'/web/recipe';
+$fields = array(
+    'id_recipe' => 175,
+    'recipe_name' => urlencode('Receita Finger 2'),
+    'description' => urlencode('esta é minha receita'),
+    'recipe' => array(
+        array('ndbno' => '01009', 'unit' => 'gr'),
+        array('ndbno' => '010010', 'unit' => 'kg')
+    )
+);
+
+//url-ify the data for the POST
+$fields_string = '';
+foreach($fields as $key => $value) { 
+    if ('recipe' == $key) {
+        foreach ($value as $key2 => $value2) {
+            $fields_string .= 'ndbno[]='.$value2['ndbno'].'&unit[]='.$value2['unit'].'&'; 
+        }
+    } else {
+        $fields_string .= $key.'='.$value.'&'; 
+    }
+}
+rtrim($fields_string, '&');
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, count($fields));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+
+//execute post
+$result = curl_exec($ch);
+
+//close connection
+curl_close($ch);
+echo $result;
